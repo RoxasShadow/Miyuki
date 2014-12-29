0773314ce0acc830bad123b6089b57a062d5baba
RoxasShadow <webmaster@giovannicapuano.net>
2014-12-29 15:19:21 +0100

    Fix tests for config reloading

--- features/step_definitions/load_config_step.rb
+++ features/step_definitions/load_config_step.rb
@@ -17,9 +17,18 @@ end
 And(/^I modify something in "(.*?)"/) do |file|
   @previous_config = Miyuki.config
 
-  FileUtils.touch(file)
+  config = File.read(file)
+
+  if config.include?('6s')
+    config.gsub!('6s', '5s')
+  elsif config.include?('5s')
+    config.gsub!('5s', '6s')
+  end
+
+  File.open(file, ?w) { |f| f.puts(config) }
 end
 
 Then(/^Miyuki reloads her configuration/) do
   expect(Miyuki.config).to_not be equal(@previous_config)
+  expect(Miyuki.config['refreshEvery']).to_not be @previous_config['refreshEvery']
 end
